0ad2770e42ae743a3cec4bca9671575681621709
Paul Copplestone <pcopplestone@gmail.com>
2021-08-03 15:33:13 +0800

    Adds company data to the job cards

--- components/JobList.tsx
+++ components/JobList.tsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from 'react'
 import { supabase } from 'lib/supabaseClient'
 import { definitions } from 'lib/definitions'
 
+type JobData = definitions['jobs'] & {
+  company: definitions['companies']
+}
+
 export default function JobList() {
-  const [jobs, setJobs] = useState<definitions['jobs'][]>([])
+  const [jobs, setJobs] = useState<JobData[]>([])
 
   useEffect(() => {
     loadPage()
@@ -11,8 +15,13 @@ export default function JobList() {
 
   const loadPage = async () => {
     const { data }: { data: any } = await supabase
-      .from<definitions['jobs']>('jobs')
-      .select('id, name')
+      .from<JobData>('jobs')
+      .select(
+        `
+        id, name, description, 
+        company:companies(id, name)
+        `
+      )
       .order('name')
     setJobs(data)
   }
@@ -20,12 +29,22 @@ export default function JobList() {
   return (
     <div className="flex flex-col w-full">
       {jobs.map((job) => (
-        <a key={job.id}>
-          <div className="border rounded py-2 mb-4">
-            <h4>{job.name}</h4>
-          </div>
-        </a>
+        <div key={job.id}>
+          <JobCard job={job} />
+        </div>
       ))}
     </div>
+  )
+}
+
+const JobCard = ({ job }: { job: JobData }) => {
+  return (
+    <a key={job.id}>
+      <div className="border rounded p-2 mb-4">
+        <h5 className="uppercase text-gray-600 text-sm">{job.company.name}</h5>
+        <h4 className="text-xl font-bold">{job.name}</h4>
+        <div>{job.description}</div>
+      </div>
+    </a>
   )
 }
