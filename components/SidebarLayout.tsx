02d56e71677ebcaf38ab843544cd4868b48466c1
Paul Copplestone <pcopplestone@gmail.com>
2021-08-03 18:45:45 +0800

    Adds some layout changes

--- components/SidebarLayout.tsx
+++ components/SidebarLayout.tsx
@@ -3,10 +3,12 @@ import SITE from 'config/site'
 import NavBar from 'components/NavBar'
 import Hero from 'components/Hero'
 import Footer from 'components/Footer'
-import { ReactChild } from 'react'
+import { ReactChild, useState } from 'react'
 import Filters from './Filters'
+import { FilterIcon, SortAscendingIcon } from '@heroicons/react/solid'
 
 export default function SidebarLayout({ children }: { children: ReactChild }) {
+  const [showFilters, setShowFilters] = useState(true)
   return (
     <div>
       {/* <header className="flex align-middle justify-center">
@@ -19,11 +21,32 @@ export default function SidebarLayout({ children }: { children: ReactChild }) {
       <NavBar />
 
       <Hero />
-      <div className="max-w-7xl mx-auto flex">
-        <nav className="p-4">
-          <Filters />
-        </nav>
-        <main className="p-4 flex flex-1">{children}</main>
+      <div className="max-w-7xl mx-auto">
+        <div className="p-4 flex gap-2">
+          <button
+            type="button"
+            onClick={() => setShowFilters(!showFilters)}
+            className="inline-flex items-center text-sm p-2 border shadow-sm text-sm leading-4 font-medium rounded "
+          >
+            Sort
+            <SortAscendingIcon className="ml-2 -mr-0.5 h-4 w-4" aria-hidden="true" />
+          </button>
+          <button
+            type="button"
+            onClick={() => setShowFilters(!showFilters)}
+            className="inline-flex items-center text-sm p-2 border shadow-sm text-sm leading-4 font-medium rounded "
+          >
+            {showFilters? 'Hide': 'Show'} Filters
+            <FilterIcon className="ml-2 -mr-0.5 h-4 w-4" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="flex">
+          <main className="p-4 flex flex-1">{children}</main>
+
+          <nav className={showFilters ? 'p-4' : 'hidden'}>
+            <Filters />
+          </nav>
+        </div>
       </div>
 
       <Footer />
